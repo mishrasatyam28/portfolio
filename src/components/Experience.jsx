59749f2ecb10c0f9b3976d4bf06eb2d1214d3cc5
Satyam Mishra <satyamsumit8750@gmail.com>
2024-02-14 16:03:18 +0530

    working in experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import reactImage from "../assets/react.png";
+import nextjs from "../assets/nextjs.png";
+import graphql from "../assets/graphql.png";
+import github from "../assets/github.png";
+import tailwind from "../assets/tailwind.png";
+import node from "../assets/node.png";
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div
+      name="experience"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div>
+          <p>Experience</p>
+          <p>These are the technologies I've worked with</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <img src={html} alt="" />
+          <p>HTML</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
-// 1:24
+// 1:27
