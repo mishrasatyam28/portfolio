6ba79ae44a94843734ad10e9fb3872afa92e9040
Satyam Mishra <satyamsumit8750@gmail.com>
2024-02-15 23:28:32 +0530

    react-typewriter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Home = () => {
+  const [text] = useTypewriter({
+    words: ["Developer", "Editor", "Designer", "Coder!"],
+    loop: Infinity,
+    onLoopDone: () => console.log(`loop completed after 3 runs.`),
+  });
   return (
     <div
       name="home"
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800  max-[500px]:h-screen "
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row  ">
         <div className="flex flex-col justify-center h-full  ">
+          <h3 className="text-4xl sm:text-7xl font-bold text-white max-[500px]:text-4xl max-[500px]:pt-6">
+            I am
+          </h3>
           <h2 className="text-4xl sm:text-7xl font-bold text-white max-[500px]:text-4xl max-[500px]:pt-4">
-            I'm a Full Stack Developer
+            {text}
+            <Cursor cursorColor="#508CE8" />
           </h2>
           <p className="text-gray-500 py-4 max-w-md max-[500px]:py-1 ">
             I have good knowledge of building and designing web application.
@@ -34,7 +44,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="bg-gradient-to-bl from-blue-800 via-blue-400 to-blue-600  max-w-80  rounded-2xl md:w-full max-[500px]:w-2/3 max-[500px]:-mt-8">
+        <div className="bg-gradient-to-bl from-blue-800 via-blue-400 to-blue-600  max-w-80  rounded-2xl md:w-full max-[500px]:w-2/3 max-[500px]:-mt-8 lg:ml-20 md:ml-6">
           <img
             className="rounded-2xl mx-auto "
             src={HeroImage}
