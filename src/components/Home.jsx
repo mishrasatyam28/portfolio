a4b5dbd9e07713a9764f978691681f4b9b15793f
Satyam Mishra <satyamsumit8750@gmail.com>
2024-02-15 22:15:33 +0530

    fix some bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800  max-[500px]:h-screen "
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white ">
+        <div className="flex flex-col justify-center h-full  ">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white max-[500px]:text-4xl ">
             I'm a Full Stack Developer
           </h2>
-          <p className="text-gray-500 py-4 max-w-md">
+          <p className="text-gray-500 py-4 max-w-md max-[500px]:py-1 ">
             I have good knowledge of building and designing web application.
             Currently, I love to work on web application using technologies like
             React, Tailwind and Nodejs.
@@ -24,7 +24,7 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer  "
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer   "
             >
               Portfolio
               <span className="group-hover:rotate-90">
@@ -34,7 +34,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="bg-gradient-to-bl from-blue-800 via-blue-400 to-blue-600  max-w-80  rounded-2xl md:w-full">
+        <div className="bg-gradient-to-bl from-blue-800 via-blue-400 to-blue-600  max-w-80  rounded-2xl md:w-full max-[500px]:w-1/2">
           <img
             className="rounded-2xl mx-auto"
             src={HeroImage}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed max-[500px]:h-15 ">
       <div>
         <h1 className="text-5xl font-signature ml-2">Satyam</h1>
       </div>
